520171fdd22cd64e5995d58bdc671f0bd752a409
gshinohara <k.tneruneru@gmail.com>
2023-05-22 01:03:32 +0900

    パラメータクラスの初期設定

---
+++ GHClippingPlane/ClippingPlaneGoo.cs
@@ -0,0 +1,37 @@
+﻿using Rhino.Geometry;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+namespace GHClippingPlane
+{
+    public class ClippingPlaneGoo : GH_GeometricGoo<ClippingPlaneSurface>
+    {
+        #region 継承Properties
+        public override BoundingBox Boundingbox => throw new System.NotImplementedException();
+        public override string TypeDescription => throw new System.NotImplementedException();
+        public override string TypeName => throw new System.NotImplementedException();
+        #endregion
+        #region 継承Method
+        public override IGH_GeometricGoo DuplicateGeometry()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override BoundingBox GetBoundingBox(Transform xform)
+        {
+            throw new System.NotImplementedException();
+        }
+        public override IGH_GeometricGoo Morph(SpaceMorph xmorph)
+        {
+            throw new System.NotImplementedException();
+        }
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override IGH_GeometricGoo Transform(Transform xform)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ GHClippingPlane/ClippingPlaneParameter.cs
@@ -0,0 +1,23 @@
+﻿using Rhino.Geometry;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using System;
+using System.Collections.Generic;
+
+namespace GHClippingPlane
+{
+    public class ClippingPlaneParameter : GH_PersistentGeometryParam<ClippingPlaneGoo>
+    {
+        public ClippingPlaneParameter() : base(new GH_InstanceDescription("Clipping Plane", "CP", "", "Params", "Geometry")) { }
+
+        public override Guid ComponentGuid => new Guid("325B5A72-A52B-41B9-B28D-CFF715848425");
+        protected override GH_GetterResult Prompt_Singular(ref ClippingPlaneGoo value)
+        {
+            throw new NotImplementedException();
+        }
+        protected override GH_GetterResult Prompt_Plural(ref List<ClippingPlaneGoo> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
